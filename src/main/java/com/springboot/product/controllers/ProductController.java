d4f56c36c4afed6b6e5bdfd6489d2aaae90b863f
Felipe Louzeiro <fl.louzeiro@gmail.com>
2023-06-08 00:36:03 -0300

    feat: implementa métodos de busca get para product

--- src/main/java/com/springboot/product/controllers/ProductController.java
+++ src/main/java/com/springboot/product/controllers/ProductController.java
@@ -2,26 +2,42 @@ package com.springboot.product.controllers;
 
 import com.springboot.product.dtos.ProductRecordDto;
 import com.springboot.product.models.ProductModel;
-import com.springboot.product.repositories.ProductRepositorie;
+import com.springboot.product.repositories.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
 
     @Autowired
-    ProductRepositorie productRepositorie;
+    ProductRepository productRepository;
 
     @PostMapping("/products")
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDto, productModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(productRepositorie.save(productModel));
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts(){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id){
+        Optional<ProductModel> product0 = productRepository.findById(id);
+        if (product0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product0.get());
     }
 }
