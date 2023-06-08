d61110cbce2968bba2d659dae6716bd0a61a65c4
Felipe Louzeiro <fl.louzeiro@gmail.com>
2023-06-08 00:21:02 -0300

    feat: implementa método para criar novo product

---
+++ src/main/java/com/springboot/product/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.springboot.product.controllers;
+
+import com.springboot.product.dtos.ProductRecordDto;
+import com.springboot.product.models.ProductModel;
+import com.springboot.product.repositories.ProductRepositorie;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepositorie productRepositorie;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepositorie.save(productModel));
+    }
+}
